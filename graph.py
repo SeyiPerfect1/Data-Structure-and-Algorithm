5e78db4552935a106f8735835c8fd745f9373485
Oluseyi Adeegbe <adeegbeoluseyi@gmail.com>
2022-08-06 17:18:24 +0100

    add graph traversal to graph.py

--- graph.py
+++ graph.py
@@ -1,6 +1,3 @@
-from lib2to3.pgen2.pgen import DFAState
-
-
 class Node:
     ''' 
         The Node class represents each vertex of the graph 
@@ -146,93 +143,37 @@ print(social_book)
 
 
 
-#Graph traversal
-#BFS
-# Python3 Program to print BFS traversal
-# from a given source vertex. BFS(int s)
-# traverses vertices reachable from s.
-# This class represents a directed graph
-# using adjacency list representation
-class Graph:
- 
-    # Constructor
-    def __init__(self):
- 
-        # default dictionary to store graph
-        self.graph = defaultdict(list)
- 
-    # function to add an edge to graph
-    def addEdge(self,u,v):
-        self.graph[u].append(v)
- 
-    # Function to print a BFS of graph
-    def BFS(self, s):
- 
-        # Mark all the vertices as not visited
-        visited = [False] * (max(self.graph) + 1)
- 
-        # Create a queue for BFS
-        queue = []
- 
-        # Mark the source node as
-        # visited and enqueue it
-        queue.append(s)
-        visited[s] = True
- 
-        while queue:
- 
-            # Dequeue a vertex from
-            # queue and print it
-            s = queue.pop(0)
-            print (s, end = " ")
- 
-            # Get all adjacent vertices of the
-            # dequeued vertex s. If a adjacent
-            # has not been visited, then mark it
-            # visited and enqueue it
-            for i in self.graph[s]:
-                if visited[i] == False:
-                    queue.append(i)
-                    visited[i] = True
- 
-# Driver code
- 
-# Create a graph given in
-# the above diagram
-g = Graph()
-g.addEdge(0, 1)
-g.addEdge(0, 2)
-g.addEdge(1, 2)
-g.addEdge(2, 0)
-g.addEdge(2, 3)
-g.addEdge(3, 3)
- 
-print ("Following is Breadth First Traversal"
-                  " (starting from vertex 2)")
-g.BFS(2)
-
-
-
-# DFS
-# Using a Python dictionary to act as an adjacency list
-graph = {
-  '5' : ['3','7'],
-  '3' : ['2', '4'],
-  '7' : ['8'],
-  '2' : [],
-  '4' : ['8'],
-  '8' : []
-}
-
-visited = set() # Set to keep track of visited nodes of graph.
-
-def dfs(visited, graph, node):  #function for dfs 
+visited = set()
+def DFS(visited, graph, node):
     if node not in visited:
-        print (node)
         visited.add(node)
+        print(node, end=" ")
         for neighbour in graph[node]:
-            dfs(visited, graph, neighbour)
+            DFS(visited, graph, neighbour)
 
-# Driver Code
-print("Following is the Depth-First Search")
-dfs(visited, graph, '5')
+
+queue = []
+visited = []
+def BFS(visited, graph, node):
+    visited.append(node)
+    queue.append(node)
+    while queue:
+        s = queue.pop(0)
+        print(s, end=" ")
+        for neighbour in graph[s]:
+            visited.append(neighbour)
+            queue.append(neighbour)
+
+
+
+# mygraph = {
+#     "A": ["B", "C"],
+#     "B": ["D", "E"],
+#     "C": ["F"],
+#     "D": [],
+#     "E": ["F"],
+#     "F": []
+# }
+# BFS(visited, mygraph, "A")
+# DFS(visited, mygraph, "A")
+
